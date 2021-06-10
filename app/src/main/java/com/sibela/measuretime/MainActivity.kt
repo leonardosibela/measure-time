5faa703756f58d98dbd7c40447bbc35ba88e3434
leonardosibela <leonardo_sibela@hotmail.com>
2021-06-09 22:13:18 -0300

    Measuring time in milliseconds

--- app/src/main/java/com/sibela/measuretime/MainActivity.kt
+++ app/src/main/java/com/sibela/measuretime/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.sibela.measuretime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        measureTimeMillis {
+            setContentView(R.layout.activity_main)
+        }.let { timeMillis ->
+            print("It took $timeMillis milliseconds.")
+        }
     }
 }
